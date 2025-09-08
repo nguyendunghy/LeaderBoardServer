2f6d93723bb8580f2d315499ff6f62cf5e100886
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-08 23:17:12 +0700

    init project

---
+++ src/main/java/com/dn/leaderboardserver/LeaderBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.dn.leaderboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderBoardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaderBoardServerApplication.class, args);
+    }
+
+}
