9a57b191beb74126149c1575fa161315e299fc92
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-12 23:24:15 +0700

    update terraform script

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,11 @@
 terraform {
+  required_version = ">= 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.20.0"
     }
   }
-  required_version = ">= 1.4.0"
-
   backend "s3" {
     bucket         = "leaderboard-terraform-states"   # thay bằng bucket của bạn
     key            = "eks/terraform.tfstate"
