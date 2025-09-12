b54257a4a3ad0817a0320d21340a3019ab33c387
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-12 22:54:40 +0700

    Update S3 bucket name

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.4.0"
 
   backend "s3" {
-    bucket         = "my-terraform-states"   # thay bằng bucket của bạn
+    bucket         = "leaderboard-terraform-states"   # thay bằng bucket của bạn
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "terraform-locks"       # bảng DynamoDB để lock state
+    dynamodb_table = "leaderboard-terraform-locks"       # bảng DynamoDB để lock state
     encrypt        = true
   }
 }
