7e50b8ac291807216a643e1b98fdd577fae68dd6
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-12 23:27:44 +0700

    update EKS module version

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ module "vpc" {
 # EKS module
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "20.24.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
