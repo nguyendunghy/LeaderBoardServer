2f8c02f7a46a43e8c984474b6e4979fdca7d1939
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-13 23:25:46 +0700

    add api get health of this system

---
+++ src/main/java/com/dn/leaderboardserver/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.dn.leaderboardserver.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String sayGood() {
+        return "good";
+    }
+
+}
---
+++ src/test/java/com/dn/leaderboardserver/controller/HealthCheckControllerTest.java
@@ -0,0 +1,25 @@
+package com.dn.leaderboardserver.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthCheckController.class)
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnGood() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("good"));
+    }
+}
