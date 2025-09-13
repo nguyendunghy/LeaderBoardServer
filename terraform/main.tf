20f76b4713c52a03fbbcdd61fbf6d00cefd7f823
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-13 16:43:27 +0700

    commen cluster_name and cluster_version

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,9 @@ module "vpc" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+#
+#   cluster_name    = var.cluster_name
+#   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
