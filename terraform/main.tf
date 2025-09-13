bc0ead94110bda5dc5b168553b4356671690de41
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-13 17:02:25 +0700

    update name of cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0"
 #
-#   cluster_name    = var.cluster_name
+  name    = var.cluster_name
 #   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
