6b25dab86fb6a840b58e005f017e414ffa21010f
dungnguyen <dung.kstncnttk56@gmail.com>
2025-09-15 23:25:44 +0700

    add default value for terraform variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,16 +9,19 @@ variable "app_name" {
 variable "docker_image" {
   description = "Docker image to deploy (from DockerHub or ECR)"
   type        = string
+  default     = "dockerdungkstn/leaderboard-app:latest"
 }
 
 variable "vpc_id" {
   description = "VPC ID where ECS will run"
   type        = string
+  default     = "vpc-dungnv93-leaderboard"
 }
 
 variable "subnets" {
   description = "List of subnets (must be in the given VPC)"
   type        = list(string)
+  default     = ["subnet-dungnv93-1", "subnet-dungnv93-2"]
 }
 
 variable "desired_count" {
